2fa24e75ea4d3bbef2da43ee10678da3c24d33c2
Joseph Doba <josephdoba@gmail.com>
2022-09-06 23:58:19 -0700

    Implement model validations. Address run time error messages

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,4 +1,9 @@
 class Employee < ActiveRecord::Base
+  # https://edgeguides.rubyonrails.org/active_record_basics.html#validations
+  # https://edgeguides.rubyonrails.org/active_record_validations.html#numericality
   belongs_to :store
+
+  validates :first_name, :last_name, :store, presense: true
+  validates :hourly_rate, numericality: { in: 40..200 }
 
 end
--- lib/store.rb
+++ lib/store.rb
@@ -1,4 +1,18 @@
+require_relative 
 class Store < ActiveRecord::Base
+  # https://edgeguides.rubyonrails.org/active_record_basics.html#validations
+  # https://edgeguides.rubyonrails.org/active_record_validations.html#numericality
+
   has_many :employees
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validate :check_mens_or_womens_apparel
+
+  def check_mens_or_womens_apparel
+    if !mens_apparel or !womens_apparel
+      errors.add(:check_mens_or_womens_apparel, "Cannot have both men and womens lines set to false. One must be true")
+    end
+  end
+
 
 end
