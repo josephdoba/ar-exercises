7f6e8410e88b9ea9264e498855fdcc7b88dfbc40
Joseph Doba <josephdoba@gmail.com>
2022-09-06 20:00:56 -0700

    Create store data into db

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -1,6 +1,11 @@
 require_relative '../setup'
 
+# https://edgeguides.rubyonrails.org/active_record_basics.html#creating-active-record-models
 puts "Exercise 1"
 puts "----------"
 
-# Your code goes below here ...
+richmond = Store.create(name: "Burnaby", annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
+burnaby = Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
+gastown = Store.create(name: "Burnaby", annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
+
+puts Store.count
