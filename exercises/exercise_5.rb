5468a542335b706f6932d3d31db1bbe1894006e6
Joseph Doba <josephdoba@gmail.com>
2022-09-06 20:49:13 -0700

    AR calculation exercise

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -7,4 +7,8 @@ require_relative './exercise_4'
 puts "Exercise 5"
 puts "----------"
 
-# Your code goes here ...
+# https://api.rubyonrails.org/classes/ActiveRecord/Calculations.html
+
+puts Store.sum("annual_revenue")
+puts Store.average("annual_revenue")
+puts Store.where(annual_revenue: 1000000..10000000).count
