43a7e3685ab13eca05617ae6371bfa6d46e58ff8
Joseph Doba <josephdoba@gmail.com>
2022-09-06 21:50:35 -0700

    create employee's and edit store and employee classes

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -8,4 +8,10 @@ require_relative './exercise_5'
 puts "Exercise 6"
 puts "----------"
 
-# Your code goes here ...
+# https://edgeguides.rubyonrails.org/active_record_basics.html#creating-active-record-models
+
+@store1.employees.create(first_name: "Thing", last_name: "One", hourly_rate: 100)
+@store1.employees.create(first_name: "Thing", last_name: "Two", hourly_rate: 100)
+@store1.employees.create(first_name: "Thing", last_name: "Three", hourly_rate: 100)
+@store2.employees.create(first_name: "Thing", last_name: "Four", hourly_rate: 100)
+@store2.employees.create(first_name: "Thing", last_name: "Five", hourly_rate: 100)
