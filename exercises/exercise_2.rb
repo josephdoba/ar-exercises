8b2ad741ffc48e2e01a05c99f4283c85668d1b85
Joseph Doba <josephdoba@gmail.com>
2022-09-06 20:16:52 -0700

    assign store id's to instance var's and change the name of @store1

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -4,4 +4,9 @@ require_relative './exercise_1'
 puts "Exercise 2"
 puts "----------"
 
-# Your code goes here ...
+# https://edgeguides.rubyonrails.org/active_record_basics.html#read
+
+@store1 = Store.find_by(id: 1)
+@store2 = Store.find_by(id: 2)
+
+@store1.name = "Ushuaia"
